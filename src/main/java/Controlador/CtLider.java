e5142d41f3addf26968222b426f90ac8ff3232a1
ErickLadino <erick_ladino@hotmail.com>
2022-08-10 10:32:24 -0500

    Reto5

---
+++ src/main/java/Controlador/CtLider.java
@@ -0,0 +1,31 @@
+package Controlador;
+
+import Clases.Lider;
+import Modelo.MdLider;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+
+public class CtLider {
+
+    MdLider mdlider;
+
+    public CtLider() {
+        this.mdlider = new MdLider();
+    }
+
+       
+    public ArrayList<Lider> listarLideres(){
+        
+        ArrayList<Lider> resultado = new ArrayList<>();
+        try {
+            resultado = this.mdlider.generarReporteLideres();
+        } catch (Exception e) {
+
+        }
+        
+        return resultado;
+        
+    }
+
+}
---
+++ src/main/java/Controlador/CtProyecto.java
@@ -0,0 +1,32 @@
+
+package Controlador;
+
+import Clases.Lider;
+import Clases.Proyecto;
+import Modelo.MdProyecto;
+import java.util.ArrayList;
+
+
+public class CtProyecto {
+    MdProyecto mdproyecto;
+
+    public CtProyecto() {
+        this.mdproyecto = new MdProyecto();
+    }
+    
+    
+    //Método que llama al modelo y en un array guarda lo que arroja la consulta de proyectos
+     public ArrayList<Proyecto> listarProyecto(){
+        
+        ArrayList<Proyecto> resultado = new ArrayList<>();
+        try {
+            resultado = this.mdproyecto.generarReporteProyectos();
+        } catch (Exception e) {
+
+        }        
+        return resultado;
+        
+    }
+    
+    
+}
